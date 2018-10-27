671f8a60ab39f1f7a5c2c8269fc396ba4cf8aa04
Kei-Tachibana <kei5kei@icloud.com>
2018-10-27 19:32:52 +0900

    Edit messages_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,10 +5,19 @@ class MessagesController < ApplicationController
     end
 
     def create
+        @message = @group.messages.new(message_params)
+          if @message.save
+              redirect_to group_messages_path(@group), notice: "Your message was sent successfully."
+          else
+              @messages = @group.messages.include(:user)
+              flash.now[:alert] = "Enter a message."
+              render :index
+          end
     end
 
     private
     def message_params
+        params.require(:message).permit(:body)
     end
 
     def set_group
