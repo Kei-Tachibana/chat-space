4a34c19a1156a3dbcf6ef3122dd779626979973f
Kei-Tachibana <kei5kei@icloud.com>
2018-11-04 20:01:03 +0900

    Set locale correctly

--- config/application.rb
+++ config/application.rb
@@ -6,18 +6,19 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module ChatSpace
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    #config.i18n.available_locales = :ja
-    config.i18n.default_locale = :ja
     config.generators do |g|
         g.stylesheets false
         g.javascripts false
         g.helper false
         g.test_framework false
     end
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ja
   end
 end
--- spec/models/messages_spec.rb
+++ spec/models/messages_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe Message, type: model do
+describe Message do
     describe '#create' do
         #メッセージが保存できた場合
         context 'can save' do
@@ -26,21 +26,21 @@ describe Message, type: model do
               it 'is invalid without body and image' do
                   message = build(:message, body: nil, image: nil)
                   message.valid?
-                  expect(message.errors[:body]).to include("を入力してください")
+                  expect(message.errors[:body]).to include("blank")
               end
 
               #group_idがないと保存できない
               it 'is invalid without group_id' do
                   message = build(:message, group_id: nil)
                   message.valid?
-                  expect(message.errors[:group]).to include("を入力してください")
+                  expect(message.errors[:group]).to include("must exist")
               end
 
               #user_idがないと保存できない
               it 'is invalid without user_id' do
                   message = build(:message, user_id: nil)
                   message.valid?
-                  expect(message.errors[:user]).to include("を入力してください")
+                  expect(message.errors[:user]).to include("must exist")
               end
         end
     end
