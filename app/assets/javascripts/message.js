7d5ad3da6f584bb04464e43085cc04e6cd5fbe14
Kei-Tachibana <kei5kei@icloud.com>
2018-11-06 20:48:11 +0900

    Create API files for asynchronous connection

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,50 @@
+$(function(){
+  //messageを追加する
+  function appendMessage(data) {
+    //imageデータの有無で表示内容を分岐する
+    if (data.image.url){
+      let imageUrl = data.image.url;
+      let html = `<div class="upper-message">
+                    <div class="upper-message__user-name">
+                        data.name
+                    </div>
+                    <div class="upper-message__date">
+                      data.created_at.strftime("%Y/%m/%d %H:%M")
+                    </div>
+                  </div>
+                  <div class="lower-message">
+                    <p class="lower-message__content">
+                      data.body
+                    </p>
+                    <img src="'imageUrl'" class="lower-message__image">
+                  </div>`
+    } else {
+      let html = `<div class="upper-message">
+                    <div class="upper-message__user-name">
+                        data.name
+                    </div>
+                    <div class="upper-message__date">
+                      data.created_at.strftime("%Y/%m/%d %H:%M")
+                    </div>
+                  </div>
+                  <div class="lower-message">
+                    <p class="lower-message__content">
+                      data.body
+                    </p>
+                  </div>`
+    }
+    $('.message').append(html);
+  };
+
+  //@messagesを取得する
+  function getMessages() {
+    let messageNumber = $('.messages').length;
+    let num = { "id": messageNumber };
+      $.ajax({
+        url:
+        type:
+        dataType:
+        data:
+      })
+  }
+})
