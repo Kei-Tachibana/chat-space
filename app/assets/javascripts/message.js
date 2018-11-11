712c6863b49bad4856dfac8d2f85a5bbd5eedbb6
Kei-Tachibana <kei5kei@icloud.com>
2018-11-11 15:27:28 +0900

    fix query

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,38 +1,34 @@
 $(function() {
-  function appendMessage(message) {
-    let messageHTML = "";
+  function buildHTML(message) {
     let imageHTML = "";
-    if (message.body) {
-      messageHTML += message.body;
-      console.log(messageHTML);
-    }
     if (message.image) {
-      imageHTML += `<img src=${message.image}>`
-      console.log(imageHTML);
+      imageHTML += `<img src="${message.image}">`;
     }
-    let html = `<div class="upper-message">
-                  <div class="upper-message__user-name">
+    let html = `<div class="message" data-id="${message.id}">
+                  <div class="upper-message">
+                    <div class="upper-message__user-name">
                       ${message.name}
+                    </div>
+                    <div class="upper-message__date">
+                      ${message.created_at}
+                    </div>
                   </div>
-                  <div class="upper-message__date">
-                    ${message.created_at}
+                  <div class="lower-message">
+                    <p class="lower-message__content">
+                      ${message.body}
+                    </p>
+                      ${imageHTML}
                   </div>
-                </div>
-                <div class="lower-message">
-                  <p class="lower-message__content">
-                    ${messageHTML}
-                  </p>
-                  ${imageHTML}
-                </div>`
+                </div>`;
     return html;
-}
+  }
 
-  $('.message-form').on ('submit', function(e) {
+  $(".message-form").on("submit", function(e){
     e.preventDefault();
+    let $list = $(".message-list")
     let formData = new FormData(this);
-    let href = $(this).attr('action');
     $.ajax({
-      url: href,
+      url: $(".message-form").attr("action"),
       type: "POST",
       data: formData,
       dataType: 'json',
@@ -40,11 +36,16 @@ $(function() {
       contentType: false
     })
     .done(function(data){
-      appendMessage(data);
-      $('.message-list').animate( { scrollTop: $('.message-list')[0].scrollHeight } );
+      let newMessage = buildHTML(data)
+      $list.append(newMessage)
+      $list.animate({scrollTop: $list[0].scrollHeight});
+      $(".message-form")[0].reset();
     })
     .fail(function(){
       alert("Server Connection Error");
-    });
+    })
+    .always(function(){
+      $(".form__submit").prop("disabled", false);
+    })
   });
 });
